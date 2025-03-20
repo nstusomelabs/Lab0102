b6e506faaedc51389628b9406c31ac9d7034d0cc
Kuso0taku <denustuzan@denustuzan.com>
2025-03-20 08:50:00 +0700

    fixed merge

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-<<<<<<< HEAD
+#include <algorithm>
 using namespace std;
 
 // convert string to lower or to upper case
@@ -8,56 +8,28 @@ void tolower_toupper(string &str, bool up_down) {
   for (auto &ch: str) {ch = (up_down?toupper(ch):tolower(ch));}
 }
 
-void print(string &str) {
-  for (auto ch: str) {cout << ch;}
-  cout << endl;
-}
-
-int main(void) {
-  cout << "type some string: ";
-  string str = "";
-  getline(cin, str);
-  cout << endl;
-  cout << "what d'ya want?\n0 - to lower case, 1 - to upper case\ntype here: ";
-  bool up_down = 1;
-  cin >> up_down;
-  cout << endl;
-  tolower_toupper(str, 1);
-  cout << "result string is: ";
-  print(str);
-  return 0;
-=======
-#include <algorithm>
-
 bool isPalindrome(const std::string& str) {
     std::string cleanedStr;
     
     // Удаляем пробелы и приводим строку к нижнему регистру
     for (char ch : str) {
-        if (std::isalnum(ch)) { // Проверяем, является ли символ буквой или цифрой
-            cleanedStr += std::tolower(ch);
+        if (isalnum(ch)) { // Проверяем, является ли символ буквой или цифрой
+            cleanedStr += tolower(ch);
         }
     }
 
     // Проверяем, является ли строка палиндромом
-    std::string reversedStr = cleanedStr;
-    std::reverse(reversedStr.begin(), reversedStr.end());
+    string reversedStr = cleanedStr;
+    reverse(reversedStr.begin(), reversedStr.end());
     
     return cleanedStr == reversedStr;
 }
 
+void print(string str) {
+  for (auto ch: str) {cout << ch;}
+  cout << endl;
+}
+
 int main() {
-    std::string input;
-    
-    std::cout << "Введите строку: ";
-    std::getline(std::cin, input);
-    
-    if (isPalindrome(input)) {
-        std::cout << "Строка является палиндромом!" << std::endl;
-    } else {
-        std::cout << "Строка не является палиндромом." << std::endl;
-    }
-    
     return 0;
->>>>>>> origin/palindrome2
 }
