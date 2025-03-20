10015f9f568f591c1451ba5dbd820cb6f992f21e
Kuso0taku <denustuzan@denustuzan.com>
2025-03-20 20:20:27 +0700

    Workable program

--- main.cpp
+++ main.cpp
@@ -3,13 +3,19 @@
 #include <algorithm>
 using namespace std;
 
+enum Mode {
+  TOLOWER_CASE,
+  TOUPPER_CASE,
+  PALINDROME
+};
+
 // convert string to lower or to upper case
 void tolower_toupper(string &str, bool up_down) {
   for (auto &ch: str) {ch = (up_down?toupper(ch):tolower(ch));}
 }
 
-bool isPalindrome(const std::string& str) {
-    std::string cleanedStr;
+bool isPalindrome(const string& str) {
+    string cleanedStr;
     
     // Удаляем пробелы и приводим строку к нижнему регистру
     for (char ch : str) {
@@ -31,5 +37,35 @@ void print(string str) {
 }
 
 int main() {
-    return 0;
+  cout << "Enter string: ";
+  string str;
+  getline(cin, str);
+  cout << "What d'ya want to do?\n(0) to convert string to lower case\n(1) to convert string to upper case\n(2) to check if string is palindrome\nEnter: ";
+ 
+  string input;
+  getline(cin, input);
+  if (input.length() == 1 && isdigit(input[0])) {
+    int mode_value = input[0] - '0';
+    if (mode_value >= TOLOWER_CASE && mode_value <= PALINDROME) {
+      Mode mode = static_cast<Mode>(mode_value);
+      switch (mode) {
+        case TOLOWER_CASE:
+          tolower_toupper(str, 0);
+          cout << "Result string is ";
+          print(str);
+          break;
+        case TOUPPER_CASE:
+          tolower_toupper(str, 1);
+          cout << "Result string is ";
+          print(str);
+          break;
+        case PALINDROME:
+          bool palindrome = isPalindrome(str);
+          cout << "String is " << (palindrome?"":"not ") << "palindrome" << endl;
+          break;
+      }
+    } else {cout << "This mode doesn't exist. Try 0, 1 or 2.";}
+  } else {cout << "Ah, I don't understand! Wrong input!";}
+
+  return 0;
 }
